69277b650e087ff4a1c54ea496d2b5c31d283584
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-08-15 13:45:57 +0200

    Initial Commit

---
+++ ColorApp_SwiftUI/ColorApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp_SwiftUIApp.swift
+//  ColorApp_SwiftUI
+//
+//  Created by Сергей Захаров on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
